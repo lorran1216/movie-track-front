f943d62c697af13c7a8b2f86a72b792a42b1c310
lorran1216 <lorranrs1216@gmail.com>
2024-03-08 17:42:10 -0300

    trying some css issues on movievue

--- src/components/MovieGenresCard.vue
+++ src/components/MovieGenresCard.vue
@@ -1,9 +1,8 @@
 <template>
-    <div class="flex flex-row w-full gap-2 bg-[#EBF8F8] rounded-xl overflow-hidden">
-        <img :src="cover" class="w-28 h-28 rounded-s-xl" />
-        <div class="flex flex-col space-y-1 max-h-[112px]">
+    <div class="container rounded-xl">
+        <img :src="cover" class="w-20 h-20 rounded-s-xl" />
+        <div class="flex flex-col space-y-5 max-h-[112px]">
             <p class="text-sm font-bold">{{ title }}</p>
-            <p class="text-xs text-ellipsis"> {{ description }}</p>
             <p class="text-xs">{{ duration }} Min</p>
         </div>
     </div>
@@ -19,9 +18,19 @@ defineProps({
         type: String,
         default: "Mock Title"
     },
-    description: String,
     duration: Number,
     cover: String
 })
 
+/*flex flex-row w-full gap-2 bg-[#EBF8F8] rounded-xl overflow-hidden*/ 
 </script>
+
+<style scoped>
+    .container{
+        display:flex;
+        flex-direction: row;
+        width: 250px;
+        background-color: #EBF8F8;
+        border-radius: 5px;
+    }
+</style>
--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -4,25 +4,22 @@
     
     import { reactive, onMounted, computed } from 'vue'
 
-    const genres = reactive({})
     const movies = reactive({})
     const media_genres = reactive({})
-    const medias = reactive({})
-
+    
 
     onMounted(async () => {
-        const response_g = await axios.get('http://localhost/api/genre')
-        genres.value = response_g.data
-
         const response_m = await axios.get('http://localhost/api/movies')
         movies.value = response_m.data
 
         const response_mg = await axios.get('http://localhost/api/mediagenre')
         media_genres.value = response_mg.data
-
-        const response_me = await axios.get('http://localhost/api/media')
-        medias.value = response_me.data
     })
+
+
+    /* slidecontainer = scroll-snap-type: x mandatory;
+        slide scroll-snap-align: start;
+        */
 
 </script>
 
@@ -30,130 +27,120 @@
 
     
     <v-container class="bg-[#263537]">
-        <h1>Fugiat.</h1>
+        <h2>Fugiat</h2>
         <div v-for="media_genre in media_genres.value">
             <div v-if="media_genre.genre_id === 1 && media_genre.id <= 10">
                 <div v-for="movie in movies.value">
                     <MovieGenresCard v-if="movie.media.id === media_genre.id" 
                     :title="movie.media.title" 
-                    :description="movie.media.description" 
                     :duration="movie.duration"
                     :cover="movie.media.cover" />
                 </div>
             </div>
         </div>
 
-        <h1>Ipsam et.</h1>
-        <div v-for="media_genre in media_genres.value">
-            <div v-if="media_genre.genre_id === 2 && media_genre.id <= 10">
-                <div v-for="movie in movies.value">
+        <h2>Ipsam et</h2>
+        <div class="catalogue-container" v-for="media_genre in media_genres.value">
+            <div class="slide-container" v-if="media_genre.genre_id === 2 && media_genre.id <= 10">
+                <div class="slide" v-for="movie in movies.value">
                     <MovieGenresCard v-if="movie.media.id === media_genre.id" 
                     :title="movie.media.title" 
-                    :description="movie.media.description" 
                     :duration="movie.duration"
                     :cover="movie.media.cover" />
                 </div>
             </div>
         </div>
 
-        <h1>Cumque.</h1>
-        <div v-for="media_genre in media_genres.value">
-            <div v-if="media_genre.genre_id === 3 && media_genre.id <= 10">
-                <div v-for="movie in movies.value">
+        <h2>Cumque</h2>
+        <div class="catalogue-container" v-for="media_genre in media_genres.value">
+            <div class="slide-container" v-if="media_genre.genre_id === 3 && media_genre.id <= 10">
+                <div class="slide" v-for="movie in movies.value">
                     <MovieGenresCard v-if="movie.media.id === media_genre.id" 
                     :title="movie.media.title" 
-                    :description="movie.media.description" 
                     :duration="movie.duration"
                     :cover="movie.media.cover" />
                 </div>
             </div>
         </div>
 
-        <h1>In autem.</h1>
-        <div v-for="media_genre in media_genres.value">
-            <div v-if="media_genre.genre_id === 4 && media_genre.id <= 10">
-                <div v-for="movie in movies.value">
+        <h2>In autem</h2>
+        <div class="catalogue-container" v-for="media_genre in media_genres.value">
+            <div class="slide-container" v-if="media_genre.genre_id === 4 && media_genre.id <= 10">
+                <div class="slide" v-for="movie in movies.value">
                     <MovieGenresCard v-if="movie.media.id === media_genre.id" 
                     :title="movie.media.title" 
-                    :description="movie.media.description" 
                     :duration="movie.duration"
                     :cover="movie.media.cover" />
                 </div>
             </div>
         </div>
 
-        <h1>Qui.</h1>
-        <div v-for="media_genre in media_genres.value">
-            <div v-if="media_genre.genre_id === 5 && media_genre.id <= 10">
-                <div v-for="movie in movies.value">
+        <h2>Qui</h2>
+        <div class="catalogue-container" v-for="media_genre in media_genres.value">
+            <div class="slide-container" v-if="media_genre.genre_id === 5 && media_genre.id <= 10">
+                <div class="slide" v-for="movie in movies.value">
                     <MovieGenresCard v-if="movie.media.id === media_genre.id" 
                     :title="movie.media.title" 
-                    :description="movie.media.description" 
                     :duration="movie.duration"
                     :cover="movie.media.cover" />
                 </div>
             </div>
         </div>
 
-        <h1>Dolores.</h1>
-        <div v-for="media_genre in media_genres.value">
-            <div v-if="media_genre.genre_id === 6 && media_genre.id <= 10">
-                <div v-for="movie in movies.value">
+        <h2>Dolores</h2>
+        <div class="catalogue-container" v-for="media_genre in media_genres.value">
+            <div class="slide-container" v-if="media_genre.genre_id === 6 && media_genre.id <= 10">
+                <div class="slide" v-for="movie in movies.value">
                     <MovieGenresCard v-if="movie.media.id === media_genre.id" 
                     :title="movie.media.title" 
-                    :description="movie.media.description" 
                     :duration="movie.duration"
                     :cover="movie.media.cover" />
                 </div>
             </div>
         </div>
 
-        <h1>Omnis.</h1>
-        <div v-for="media_genre in media_genres.value">
-            <div v-if="media_genre.genre_id === 7 && media_genre.id <= 10">
-                <div v-for="movie in movies.value">
+        <h2>Omnis</h2>
+        <div class="catalogue-container" v-for="media_genre in media_genres.value">
+            <div class="slide-container" v-if="media_genre.genre_id === 7 && media_genre.id <= 10">
+                <div class="slide" v-for="movie in movies.value">
                     <MovieGenresCard v-if="movie.media.id === media_genre.id" 
                     :title="movie.media.title" 
-                    :description="movie.media.description" 
                     :duration="movie.duration"
                     :cover="movie.media.cover" />
                 </div>
             </div>
         </div>
 
-        <h1>Mollitia.</h1>
-        <div v-for="media_genre in media_genres.value">
-            <div v-if="media_genre.genre_id === 8 && media_genre.id <= 10">
-                <div v-for="movie in movies.value">
+        <h2>Mollitia</h2>
+        <div class="catalogue-container" v-for="media_genre in media_genres.value">
+            <div class="slide-container" v-if="media_genre.genre_id === 8 && media_genre.id <= 10">
+                <div class="slide" v-for="movie in movies.value">
                     <MovieGenresCard v-if="movie.media.id === media_genre.id" 
                     :title="movie.media.title" 
-                    :description="movie.media.description" 
                     :duration="movie.duration"
                     :cover="movie.media.cover" />
                 </div>
             </div>
         </div>
 
-        <h1>Fuga qui.</h1>
-        <div v-for="media_genre in media_genres.value">
-            <div v-if="media_genre.genre_id === 9 && media_genre.id <= 10">
-                <div v-for="movie in movies.value">
+        <h2>Fuga qui</h2>
+        <div class="catalogue-container" v-for="media_genre in media_genres.value">
+            <div class="slide-container" v-if="media_genre.genre_id === 9 && media_genre.id <= 10">
+                <div class="slide" v-for="movie in movies.value">
                     <MovieGenresCard v-if="movie.media.id === media_genre.id" 
                     :title="movie.media.title" 
-                    :description="movie.media.description" 
                     :duration="movie.duration"
                     :cover="movie.media.cover" />
                 </div>
             </div>
         </div>
 
-        <h1>Corrupti.</h1>
-        <div v-for="media_genre in media_genres.value">
-            <div v-if="media_genre.genre_id === 10 && media_genre.id <= 10">
-                <div v-for="movie in movies.value">
+        <h2>Corrupti</h2>
+        <div class="catalogue-container" v-for="media_genre in media_genres.value">
+            <div class="slide-container" v-if="media_genre.genre_id === 10 && media_genre.id <= 10">
+                <div class="slide" v-for="movie in movies.value">
                     <MovieGenresCard v-if="movie.media.id === media_genre.id" 
                     :title="movie.media.title" 
-                    :description="movie.media.description" 
                     :duration="movie.duration"
                     :cover="movie.media.cover" />
                 </div>
@@ -166,8 +153,33 @@
 </template>
 
 <style scoped>
-    .cards{
-        overflow-x: auto;
-        white-space: nowrap;
+
+    h2{
+        font-size: 14px;
+        margin-right: 10px;
+        font-style: italic;
+        color:#EBF8F8;
     }
+
+    .catalogue-container{
+        width: 100%;
+        margin: auto;
+        overflow-x: auto;
+    }
+
+    .slide-container{
+        display: flex;
+        flex-direction: column;
+        -webkit-overflow-scrolling: touch;
+        white-space: nowrap; 
+    }
+
+    .slide{
+        width: 100%;
+        flex: 0 0 auto;
+        white-space: normal;
+        padding: 0 10px;
+        box-sizing: border-box;
+    }
+    
 </style>
